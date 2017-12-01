7d5b9e67d21bc362b2c8ce601f22c68e24fca06f
avarona <avarona10@gmail.com>
2017-11-30 19:56:09 -0500

    fix(db): seed file

--- db/_db.js
+++ db/_db.js
@@ -2,7 +2,7 @@
 
 const Sequelize = require('sequelize');
 
-const dbName = 'example';	// TODO: CHANGE dbName
+const dbName = 'peerdev';
 const url = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`;
 
 const db = new Sequelize(url, {
